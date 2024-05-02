3b33d744f944ab9c5a70d7840a024805173054b1
JUWON BAIK <qorwndnjs04@gmail.com>
2024-05-02 02:24:45 +0000

    Recoding

--- ANU0502/ANU0502/Stack3b.java
+++ ANU0502/ANU0502/Stack3b.java
@@ -1,4 +1,24 @@
 package ANU0502;
 
 public class Stack3b {
+    Std3b top;
+    Stack3b() {
+        top = null;
+    }
+    void push(Std3b a){
+        a.next = top;
+        top = a;
+    }
+    Std3b pop(){
+        Std3b t = top;
+        top = top.next;
+        return t;
+    }
+    void pr(){
+        Std3b t = top;
+        while(t!=null){
+            t.pr();
+            t=t.next;
+        }
+    }
 }
--- ANU0502/ANU0502/Std3b.java
+++ ANU0502/ANU0502/Std3b.java
@@ -1,4 +1,13 @@
 package ANU0502;
 
 public class Std3b {
+    int id, score;
+    String name;
+    Std3b.next;
+    Std3b(int a, String b, int c){
+        id = a; name = b; score = c; next = null;
+    }
+    void pr(){
+        System.out.println(id+" "+ name + " "+ score);
+    }
 }
